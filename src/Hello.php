bb9a627eedacbc9832d70b28927a78a0108250d9
xiaozha87 <304270703@qq.com>
2017-01-05 02:21:54 -0500

    modi test

---
+++ src/Hello.php
@@ -0,0 +1,23 @@
+<?php
+namespace Xiaozha\Composer_test;
+
+class Hello
+{
+    private $name;
+
+    public function __construct( $name = 'World' )
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function hello()
+    {
+        return 'Hello '.$this->name.'!';
+    }
+}
+
--- test.php
+++ test.php
@@ -1,2 +1,10 @@
 <?php
-phpinfo();
+require_once "vendor/autoload.php";
+
+$hello = new Xiaozha\Composer_test\Hello();
+echo $hello->hello();
+
+echo "\n";
+$hiGirl = new Xiaozha\Composer_test\Hello('My Goddess');
+echo $hiGirl->hello();
+
